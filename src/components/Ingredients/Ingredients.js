9af0bce94efe089359f177bcce4d7e377de4a8c0
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-30 13:11:53 +0200

    promise then to async await

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -10,7 +10,17 @@ function Ingredients() {
 
   const [userIngredients, setUserIngredients] = useState([])
 
-  const AddIngredientHandler = ingredient => {
+  const AddIngredientHandler = async ingredient => {
+
+
+    const response = await
+      fetch('https://react-hooks-update-7337b.firebaseio.com/ingredients.json', {
+        method: 'POST',
+        body: JSON.stringify(ingredient),
+        headers: { 'Content-type': 'application/json' }
+      })
+    await response.json();
+
     setUserIngredients((prevIngredient) => {
       return [...prevIngredient, { id: Math.random().toString(), ...ingredient }]
     })
