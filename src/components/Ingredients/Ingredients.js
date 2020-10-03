2c2cef8ef437eda1f932f2ee4552a26dd9537639
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-03 08:31:55 +0200

    add ingredient to server

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
 
 const Ingredients = () => {
-
   const [ingredients, setIngredients] = useState([])
 
   const onSetIngredients = ({ title, amount }) => {
-    setIngredients((prevState) => ([...prevState, { id: Math.random().toString(), title, amount }]))
+
+    fetch('https://react-hooks-update-7337b.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify({ title, amount }),
+      headers: { 'Content-type': 'application/json' }
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        setIngredients((prevState) => ([...prevState, { id: responseData.name, title, amount }]))
+      })
   }
 
   const onRemoveItem = (id) => {
     setIngredients(ingredients.filter((item) => item.id !== id));
   }
+
+  // useEffect(() => {
+  //   fetch('https://react-hooks-update-7337b.firebaseio.com/ingredients.json')
+  //     .then(res => res.json())
+  //     .then((res) => {
+  //       console.log(res)
+
+  //     })
+  // }, [])
 
   return (
     <div className="App">
