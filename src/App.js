28a6013153643a9e1aba9fa0ef3547c0a67f0343
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-19 12:39:21 +0200

    setting up clear project - practice react hooks !

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 
-import Navigation from './components/Nav/Navigation';
-import ProductsPage from './containers/Products';
-import FavoritesPage from './containers/Favorites';
-import Counter from './containers/Counter';
+import Ingredients from './components/Ingredients/Ingredients';
 
 const App = props => {
-  return (
-    <React.Fragment>
-      <Navigation />
-      <main>
-        <Route path="/" component={ProductsPage} exact />
-        <Route path="/favorites" component={FavoritesPage} />
-        <Counter />
-      </main>
-    </React.Fragment>
-  );
+  return <Ingredients />;
 };
 
 export default App;
