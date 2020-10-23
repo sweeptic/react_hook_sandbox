547cb2d46bab7a149c3321e0bc9454d5e2e3d4e3
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-23 18:45:37 +0200

    add useReducer hook- Using in ingredient - ADD DELETE SET actions

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useReducer, useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,7 +6,25 @@ import IngredientList from './IngredientList';
 import ErrorModal from './../UI/ErrorModal';
 
 function Ingredients() {
-  const [ingredients, setIngredients] = useState([]);
+  // const [ingredients, setIngredients] = useState([]);
+
+  const initialState = [];
+
+  const reducer = (currentState, action) => {
+    switch (action.type) {
+      case 'SET':
+        return action.ingredients;
+      case 'ADD':
+        return [...currentState, action.ingredient];
+      case 'DELETE':
+        return currentState.filter(i => i !== action.itemId);
+      default:
+        throw new Error('Should not get there');
+    }
+  };
+
+  const [componentState, dispatch] = useReducer(reducer, initialState);
+
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
@@ -20,8 +38,11 @@ function Ingredients() {
       .then(res => res.json())
       .then(({ name }) => {
         setLoading(false);
-        setIngredients(prev => [...prev, { id: name, title, amount }]);
+
+        dispatch({ type: 'ADD', ingredient: { id: name, title, amount } });
+        // setIngredients(prev => [...prev, { id: name, title, amount }]);
       })
+
       .catch(err => {
         setError(true);
         setLoading(false);
@@ -39,9 +60,11 @@ function Ingredients() {
       .then(res => res.json())
       .then(res => {
         setLoading(false);
-        setIngredients(currState =>
-          currState.filter(item => item.id !== itemId)
-        );
+
+        dispatch({ type: 'DELETE', id: itemId });
+        // setIngredients(currState =>
+        // currState.filter(item => item.id !== itemId)
+        // );
       })
       .catch(err => {
         setError(true);
@@ -53,8 +76,9 @@ function Ingredients() {
   //   setLoading(loadingState);
   // }, []);
 
-  const onSetIngredients = useCallback(ingredients => {
-    setIngredients(ingredients);
+  const onSetIngredients = useCallback(filteredIngredients => {
+    dispatch({ type: 'SET', ingredients: filteredIngredients });
+    // setIngredients(ingredients);
   }, []);
 
   const modalCloseHandler = () => {
@@ -64,11 +88,11 @@ function Ingredients() {
   const ingredientList = useMemo(() => {
     return (
       <IngredientList
-        ingredients={ingredients}
+        ingredients={componentState}
         onRemoveItem={RemoveItemHandler}
       />
     );
-  }, [ingredients, RemoveItemHandler]);
+  }, [componentState, RemoveItemHandler]);
 
   return (
     <div className='App'>
