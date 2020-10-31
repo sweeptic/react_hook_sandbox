d1cd21a359a404369c0ec30857a6a64a6896f9cd
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-31 18:27:25 +0100

    react hooks starting project

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,26 +1,20 @@
-import React, { useContext } from 'react';
-// import { useDispatch } from 'react-redux';
+import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import Card from '../UI/Card';
 import './ProductItem.css';
-// import { toggleFav } from '../../store/actions/products';
-import { ProductsContext } from './../../context/products-context';
+import { toggleFav } from '../../store/actions/products';
 
 const ProductItem = props => {
-  // const dispatch = useDispatch();
-
-  // const toggleFavHandler = () => {
-  //   dispatch(toggleFav(props.id));
-  // };
-  const toggleFav = useContext(ProductsContext).toggleFav;
+  const dispatch = useDispatch();
 
   const toggleFavHandler = () => {
-    toggleFav(props.id);
+    dispatch(toggleFav(props.id));
   };
 
   return (
     <Card style={{ marginBottom: '1rem' }}>
-      <div className='product-item'>
+      <div className="product-item">
         <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
         <p>{props.description}</p>
         <button
--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,15 +1,13 @@
-import React, { useContext } from 'react';
-// import { useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 import ProductItem from '../components/Products/ProductItem';
 import './Products.css';
-import { ProductsContext } from './../context/products-context';
 
 const Products = props => {
-  // const productList = useSelector(state => state.shop.products);
-  const productList = useContext(ProductsContext).products;
+  const productList = useSelector(state => state.shop.products);
   return (
-    <ul className='products-list'>
+    <ul className="products-list">
       {productList.map(prod => (
         <ProductItem
           key={prod.id}
