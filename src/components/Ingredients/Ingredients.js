53a064c71e97c95dc40d440d7faf4b4ec421902e
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-05 14:36:51 +0100

    fetch data in ingredients component bby useeffect hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,6 +6,22 @@ import IngredientList from './IngredientList';
 
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    const dataList = [];
+    fetch(`https://react-hooks-update-7337b.firebaseio.com/ingredients.json`)
+      .then(res => res.json())
+      .then(data => {
+        for (const key in data) {
+          dataList.push({
+            id: key,
+            name: data[key].title,
+            amount: data[key].amount,
+          });
+        }
+        setIngredients(dataList);
+      });
+  }, []);
 
   const onAddIngredientHandler = ({ name, amount }) => {
     setIngredients(prevState => {
