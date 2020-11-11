d6441ab908b97f030c163bf0b8d7a86315bdc54a
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-11 05:53:46 +0100

    commit to begin of auth-context

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Card from './UI/Card';
 import './Auth.css';
 
 const Auth = props => {
-  const loginHandler = () => {};
+  const loginHandler = () => {
+    //.....
+  };
 
   return (
-    <div className="auth">
+    <div className='auth'>
       <Card>
         <h2>You are not authenticated!</h2>
         <p>Please log in to continue.</p>
--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import AuthContextProvider from './auth-context';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+
+  document.getElementById('root')
+);
