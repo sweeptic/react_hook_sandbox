0e50f03069cb6bdec4f945dbdeffaa2423a32f20
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-15 07:38:02 +0100

    create custom store

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import configureStore from './hooks-store/product-store';
+
+configureStore();
 
 ReactDOM.render(
   <BrowserRouter>
